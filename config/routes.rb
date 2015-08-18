24eaf52b0dede4c98d448c9495f4e5bb97354b3f
Sabastian <sswbelser@gmail.com>
2015-08-18 12:30:58 -0700

    Set up home page; added seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
 
-
-
 	root "conferences#index"
-  
-  resources :users, only: [:create, :update, :index]
-  get "/signup", to: "users#new", as: :signup
-  get "/profile", to: "users#show", as: :profile
-  get "/edit-profile", to: "users#edit", as: :edit_profile
 
-  resources :sessions, only: [:create]
-  get "/login", to: "sessions#new", as: :login
-  get "/logout", to: "sessions#destroy", as: :logout
+	resources :users, only: [:create, :update, :index]
+	get "/signup", to: "users#new", as: :signup
+	get "/profile", to: "users#show", as: :profile
+	get "/edit-profile", to: "users#edit", as: :edit_profile
 
-  resources :conferences, only: [:index, :show]
+	resources :sessions, only: [:create]
+	get "/login", to: "sessions#new", as: :login
+	get "/logout", to: "sessions#destroy", as: :logout
+
+	resources :conferences, only: [:index, :show]
 
 end
