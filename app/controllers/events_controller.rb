ba1f1929e9dc3f4ab484f8613258f3cfd853e90a
Sabastian <sswbelser@gmail.com>
2015-08-18 17:38:03 -0700

    Added event show page; nothing works

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,8 +9,6 @@ class EventsController < ApplicationController
 	def create
 		# redirect user if already logged in
 		if current_user
-			redirect_to root_path
-		else
 			event = Event.new(event_params)
 			if event.save
 				flash[:notice] = "Successfully saved event."
@@ -19,17 +17,19 @@ class EventsController < ApplicationController
 				flash[:error] = event.errors.full_messages.join(', ')
 				redirect_to event_path
 			end
+		else
+			redirect_to root_path
 		end
 	end
 
 	def show
-		@event = Event.find(params[:name])
+		@event = Event.find(event_params[:name])
 		render :show
 	end
 
 	private
 		def event_params
-			params.require(:user).permit(:name, :address, :city, :state, :date_start, :date_end, :start_time, :end_time, :capacity, :cost, :catorgory_id)
+			params.require(:event).permit(:name, :address, :city, :state, :date_start, :date_end, :start_time, :end_time, :capacity, :cost, :catorgory)
 		end
 
 end
