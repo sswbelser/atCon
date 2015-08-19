7373c0a70265f78934aceead6a874b81a000cdec
goodbedford <bedford@goodbedford.com>
2015-08-18 20:12:30 -0700

    starte event error branch

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,10 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+    render :index
+  end
+
 	def new
 		@event = Event.new
 		render :new
@@ -9,6 +15,7 @@ class EventsController < ApplicationController
 	def create
 		# redirect user if already logged in
 		if current_user
+  
 			event = Event.new(event_params)
 			if event.save
 				flash[:notice] = "Successfully saved event."
@@ -22,14 +29,49 @@ class EventsController < ApplicationController
 		end
 	end
 
+  def edit
+    if current_user 
+      @event = Event.find(params[:id])
+      render :edit
+    else
+      flash[:error] = event.errors.full_messages.join(', ')
+      redirect_to root_path
+    end
+  end
+
+  def update
+    event = Event.find(params[:id])
+    if current_user
+        puts "this is event params==#{event_params}"
+       if event.update_attributes(event_params)
+         redirect_to event_path(event)
+         flash[:notice] = "Successfully updated event:#{event.name}!"
+       else
+        flash[:error] = user.errors.full_messages.join(', ')
+        redirect_to edit_event_path  
+       end
+    else
+      flash[:error] = user.errors.full_messages.join(', ')
+      redirect_to edit_event_path
+    end
+
+  end
+
 	def show
-		@event = Event.find(event_params[:name])
+		@event = Event.find(params[:id])
 		render :show
 	end
 
 	private
 		def event_params
-			params.require(:event).permit(:name, :address, :city, :state, :date_start, :date_end, :start_time, :end_time, :capacity, :cost, :catorgory)
+			params.require(:event).permit(:name, :address, :city, :state, :start_date, :end_date, :start_time, :end_time, :capacity, :cost, :category)
 		end
 
 end
+
+      # some_params = {name: event_params[:name], address: event_params[:address],city: event_params[:city],
+      # state: event_params[:state], start_date: event_params[:start_date], end_date: event_params[:end_date],
+      # start_time: event_params[:start_time], end_time: event_params[:end_time],
+      # capacity: event_params[:capacity], cost: event_params[:cost], category: event_params[:category].to_i}
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818215703) do
+ActiveRecord::Schema.define(version: 20150819024441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "conferences", force: :cascade do |t|
     t.string   "name"
