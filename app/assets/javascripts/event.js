cb88de09cc8e558df1bd4c19b767b7d7f2883927
Sabastian <sswbelser@gmail.com>
2015-08-19 21:50:49 -0700

    Still works

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
   console.log("this is working")
   $("body").on("click", ".edit-btn", function(e){
     e.preventDefault()
@@ -11,16 +11,10 @@ $(document).ready(function(){
     getNum($(this));
 
     function getNum(element){
-      //var num = $("#edit-post").attr("action")
-      //console.log("this is edit post",num);
       var num = $(element).attr("href");
       num = num.split("/");
-      console.log(num[2]);
       var actionStr = "/posts/"+num[2]
       $("#edit-post").attr("action", actionStr) 
-      console.log($("#edit-post").attr("action"));
-
     }
-
   });
 });
