13a80e95f4b618f6a18c7aea956fc76232af8ea4
goodbedford <bedford@goodbedford.com>
2015-08-20 11:57:37 -0700

    updated show events

--- db/migrate/20150820181204_create_rsvps.rb
+++ db/migrate/20150820181204_create_rsvps.rb
@@ -1,7 +1,8 @@
 class CreateRsvps < ActiveRecord::Migration
   def change
     create_table :rsvps do |t|
-
+      t.belongs_to :event
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
